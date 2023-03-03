00b2c3b92eb8431298858ab1728f2f7ec7dc12a1
Satsuki Kasiwagi <122766543+march2226@users.noreply.github.com>
2023-03-03 22:33:28 +0900

    ログインページ

--- task/src/main/java/com/example/task/data/repository/UsersRepository.java
+++ task/src/main/java/com/example/task/data/repository/UsersRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.task.data.entity.Users;
 
 // JpaRepositoryインターフェイスを継承
-public interface UsersRepository extends JpaRepository<Users, Long> {
+public interface UsersRepository extends JpaRepository<Users, String> {
 
 }
