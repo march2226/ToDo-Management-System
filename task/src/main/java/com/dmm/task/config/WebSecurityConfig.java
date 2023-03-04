018c6d5a1e4d61aa1094597cc94824d38bb1ebc5
Satsuki Kasiwagi <122766543+march2226@users.noreply.github.com>
2023-03-04 20:36:23 +0900

    途中経過

--- task/src/main/java/com/dmm/task/config/WebSecurityConfig.java
+++ task/src/main/java/com/dmm/task/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.task.config;
+package com.dmm.task.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
